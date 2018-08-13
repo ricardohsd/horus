481a1281327e413e7d21660e93f4b9bed55ebb33
Ricardo Domingos <ricardohsd@gmail.com>
2018-08-13 20:53:54 +0200

    Refactor tests with testify

--- rolling_window_test.go
+++ rolling_window_test.go
@@ -2,26 +2,21 @@ package horus
 
 import (
 	"math"
-	"reflect"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestRollingWindow_Errors(t *testing.T) {
 	_, err := NewRWindow(0*time.Second, 1*time.Second)
-	if err == nil {
-		t.Errorf("Error can't be nil. Wndow must be higher than 0.")
-	}
+	assert.NotNil(t, err, "Error can't be nil. Window must be higher than 0.")
 
 	_, err = NewRWindow(1*time.Second, 0*time.Second)
-	if err == nil {
-		t.Errorf("Error can't be nil. Granularity must be higher than 0.")
-	}
+	assert.NotNil(t, err, "Error can't be nil. Granularity must be higher than 0.")
 
 	_, err = NewRWindow(1*time.Second, 2*time.Second)
-	if err == nil {
-		t.Errorf("Error can't be nil. Window must be a multiplier of granularity.")
-	}
+	assert.NotNil(t, err, "Error can't be nil. Window must be a multiplier of granularity.")
 }
 
 func TestRollingWindow_Add(t *testing.T) {
@@ -62,10 +57,9 @@ func TestRollingWindow_Add(t *testing.T) {
 	rw.Add(25.0)
 
 	avg := math.Round(rw.Average()*100) / 100
-	total := 20.0
-	if avg != total {
-		t.Errorf("Average doesn't match. Expected %v, got %v", total, avg)
-	}
+	assert.Equal(t, 20.0, avg)
+
+	assert.Equal(t, 6, rw.Count())
 }
 
 func TestRollingWindow_MaxMin(t *testing.T) {
@@ -82,32 +76,14 @@ func TestRollingWindow_MaxMin(t *testing.T) {
 	go rw.cleanBuckets(ticker)
 	ticker.Tick()
 
-	max := rw.Max()
-	expected := 0.0
-	if max != expected {
-		t.Errorf("Max doesn't match. Expected %v, got %v", expected, max)
-	}
-
-	min := rw.Min()
-	expected = 0.0
-	if min != expected {
-		t.Errorf("Min doesn't match. Expected %v, got %v", expected, min)
-	}
+	assert.Equal(t, 0.0, rw.Max())
+	assert.Equal(t, 0.0, rw.Min())
 
 	rw.Add(200.0)
 	rw.Add(200.0)
 
-	max = rw.Max()
-	expected = 400.0
-	if max != expected {
-		t.Errorf("Max doesn't match. Expected %v, got %v", expected, max)
-	}
-
-	min = rw.Min()
-	expected = 0.0
-	if min != expected {
-		t.Errorf("Min doesn't match. Expected %v, got %v", expected, min)
-	}
+	assert.Equal(t, 400.0, rw.Max())
+	assert.Equal(t, 0.0, rw.Min())
 
 	// Tick to advance 2 positions
 	ticker.Tick()
@@ -133,23 +109,10 @@ func TestRollingWindow_MaxMin(t *testing.T) {
 
 	rw.Add(80.0)
 
-	max = rw.Max()
-	expected = 80.0
-	if max != expected {
-		t.Errorf("Max doesn't match. Expected %v, got %v", expected, max)
-	}
+	assert.Equal(t, 80.0, rw.Max())
+	assert.Equal(t, -25.0, rw.Min())
 
-	min = rw.Min()
-	expected = -25.0
-	if min != expected {
-		t.Errorf("Min doesn't match. Expected %v, got %v", expected, min)
-	}
-
-	total := rw.Count()
-	count := 7
-	if total != count {
-		t.Errorf("Count doesn't match. Expected %v, got %v", count, total)
-	}
+	assert.Equal(t, 7, rw.Count())
 }
 
 func TestRollingWindow_quit(t *testing.T) {
@@ -174,24 +137,18 @@ func TestRollingWindow_quit(t *testing.T) {
 	expected := []float64{
 		0, 400, 0, 0, 0,
 	}
-	if !reflect.DeepEqual(expected, rw.values) {
-		t.Errorf("Values don't match. Expected %v, got %v", expected, rw.values)
-	}
+	assert.Equal(t, expected, rw.values)
 
 	rw.Stop()
 
 	rw.Add(500.0)
 
-	if !reflect.DeepEqual(expected, rw.values) {
-		t.Errorf("Values shouldn't be changed after stop. Expected %v, got %v", expected, rw.values)
-	}
+	assert.Equal(t, expected, rw.values)
 }
 
 func TestRollingWindow_cleaning(t *testing.T) {
 	rw, err := NewRWindow(5*time.Second, 1*time.Second)
-	if err != nil {
-		t.Errorf("Error should be nil. Got %v", err)
-	}
+	assert.Nil(t, err)
 
 	rw.Add(10.0)
 	rw.Add(20.0)
@@ -199,8 +156,5 @@ func TestRollingWindow_cleaning(t *testing.T) {
 	// wait for the window to be over
 	time.Sleep(7 * time.Second)
 
-	avg := rw.Average()
-	if avg != 0 {
-		t.Errorf("Average should be 0, got %v", avg)
-	}
+	assert.Equal(t, 0.0, rw.Average())
 }
